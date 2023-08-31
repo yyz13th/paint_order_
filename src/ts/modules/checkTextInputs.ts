38675b3ff81d671b6b549f353e806b3ffa673d26
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-08-31 14:42:41 +0300

    disable autofill for name/message inputs

---
+++ src/ts/modules/checkTextInputs.ts
@@ -0,0 +1,18 @@
+const checkTextInputs = (selector: string) => {
+
+    const txtInputs: NodeListOf<HTMLInputElement> = document.querySelectorAll(selector);
+    const regex: RegExp = /[^а-яё 0-9]/ig;
+
+    txtInputs.forEach(input => {
+
+        input.setAttribute( "autocomplete", "__away" ); //bans autofill
+        input.addEventListener('keypress', function (e): void {
+
+            if (e.key.match(regex)) {
+                e.preventDefault();
+            }
+        })
+    });
+};
+
+export default checkTextInputs;
