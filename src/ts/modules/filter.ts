1170fe519b42524f540fe9ad08ffb79f6e210e4d
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-09-01 16:57:10 +0300

    painting display filter fixes

--- src/ts/modules/filter.ts
+++ src/ts/modules/filter.ts
@@ -1,32 +1,34 @@
 const filter = (): void => {
-    const menu: HTMLElement | null = document.querySelector('.portfolio-menu'),
-        items: NodeListOf<HTMLElement> | undefined = menu?.querySelectorAll('li'),
-        btnAll: HTMLElement | null = menu?.querySelector('.all') ?? null,
-        btnLovers: HTMLElement | null = menu?.querySelector('.lovers') ?? null,
-        btnChef: HTMLElement | null = menu?.querySelector('.chef') ?? null,
-        btnGirl: HTMLElement | null = menu?.querySelector('.girl') ?? null,
-        btnGuy: HTMLElement | null = menu?.querySelector('.guy') ?? null,
-        btnGrandmother: HTMLElement | null = menu?.querySelector('.grandmother') ?? null,
-        btnGranddad: HTMLElement | null = menu?.querySelector('.granddad') ?? null,
-        wrapper: HTMLElement | null = document.querySelector('.portfolio-wrapper'),
-        markAll: NodeListOf<HTMLElement>| undefined  = wrapper?.querySelectorAll('.all'),
-        markGirl: NodeListOf<HTMLElement>| undefined = wrapper?.querySelectorAll('.girl'),
-        markLovers: NodeListOf<HTMLElement>| undefined = wrapper?.querySelectorAll('.lovers'),
-        markChef: NodeListOf<HTMLElement>| undefined = wrapper?.querySelectorAll('.chef'),
-        markGuy: NodeListOf<HTMLElement>| undefined = wrapper?.querySelectorAll('.guy'),
-        no: HTMLElement | null = document.querySelector('.portfolio-no');
+    const menu: HTMLElement | null = document.querySelector('.portfolio-menu');
+    const items: NodeListOf<HTMLElement> | undefined = menu?.querySelectorAll('li');
+    const btnAll: HTMLElement | null = menu?.querySelector('.all') ?? null;
+    const btnLovers: HTMLElement | null = menu?.querySelector('.lovers') ?? null;
+    const btnChef: HTMLElement | null = menu?.querySelector('.chef') ?? null;
+    const btnGirl: HTMLElement | null = menu?.querySelector('.girl') ?? null;
+    const btnGuy: HTMLElement | null = menu?.querySelector('.guy') ?? null;
+    const btnGrandmother: HTMLElement | null = menu?.querySelector('.grandmother') ?? null;
+    const btnGranddad: HTMLElement | null = menu?.querySelector('.granddad') ?? null;
+    const wrapper: HTMLElement | null = document.querySelector('.portfolio-wrapper');
+    const markAll: NodeListOf<HTMLElement> | undefined = wrapper?.querySelectorAll('.all');
+    const markGirl: NodeListOf<HTMLElement> | undefined = wrapper?.querySelectorAll('.girl');
+    const markLovers: NodeListOf<HTMLElement> | undefined = wrapper?.querySelectorAll('.lovers');
+    const markChef: NodeListOf<HTMLElement> | undefined = wrapper?.querySelectorAll('.chef');
+    const markGuy: NodeListOf<HTMLElement> | undefined = wrapper?.querySelectorAll('.guy');
+    const no: HTMLElement | null = document.querySelector('.portfolio-no');
 
+// non-null assertion could be used here as well instead of chaining. in such case coalescing is not needed 
+    
     const typeFilter = (markType: NodeListOf<HTMLElement>): void => {
-        markAll ? markAll.forEach(mark => {
+        markAll ? markAll.forEach((mark: HTMLElement) => {
             mark.style.display = 'none';
             mark.classList.remove('animated', 'fadeIn');
-        }): undefined;
+        }) : undefined;
 
         no ? no.style.display = 'none' : null;
         no ? no.classList.remove('animated', 'fadeIn') : null;
 
         if (markType) {
-            markType.forEach(mark => {
+            markType.forEach((mark: HTMLElement) => {
                 mark.style.display = 'block';
                 mark.classList.add('animated', 'fadeIn');
             });
@@ -45,6 +47,8 @@ const filter = (): void => {
         }
     }
 
+    // you can simply use marks without assigning them as node list elemetns when non-assertion is used
+    
     addClickListener(btnAll, markAll as NodeListOf<HTMLElement>);
     addClickListener(btnLovers, markLovers as NodeListOf<HTMLElement>);
     addClickListener(btnGuy, markGuy as NodeListOf<HTMLElement>);
@@ -60,7 +64,7 @@ const filter = (): void => {
             //items = menu buttons
             items?.forEach(item => item.classList.remove('active'));
             target.classList.add('active');
-            
+
         }
     })
 
